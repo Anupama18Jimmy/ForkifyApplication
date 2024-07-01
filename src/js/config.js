13524c3f5b8432db0833ec42b268e57d5d8dfc0d
Anupama18Jimmy <anupamajimmy199@gmail.com>
2024-07-01 13:49:57 +0530

    Initial Commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '31eb6b6f-cc37-4773-9a68-fd53e4a2e280';
+export const MODAL_CLOSE_SEC = 2.5;
